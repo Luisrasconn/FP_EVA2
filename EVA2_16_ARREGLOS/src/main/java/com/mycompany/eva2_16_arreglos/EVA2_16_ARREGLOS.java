2d7fa4c12d16fb06e9045fdfe67fbbd08fc7afba
Luisrasconn <L24550277@chihuahua2.tecnm.mx>
2024-10-21 16:48:47 -0600

    Add files via upload

---
+++ EVA2_16_ARREGLOS/src/main/java/com/mycompany/eva2_16_arreglos/EVA2_16_ARREGLOS.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_16_arreglos;
+
+/**
+ *
+ * @author fdo_g
+ */
+public class EVA2_16_ARREGLOS {
+
+    public static void main(String[] args) {
+        String[] dias={"DOMINGO","LUNES","MARTES","MIERCOLES","JUEVES","VIERNES","SABADO"};
+        for (int i = 0; i < dias.length; i++) {
+            System.out.println(dias[i] );
+            }
+    }
+}
