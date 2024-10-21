70d190c450c62a067f3d1b8df5ca7f93fa2fb378
Luisrasconn <L24550277@chihuahua2.tecnm.mx>
2024-10-21 16:46:42 -0600

    Add files via upload

---
+++ EVA2_12_TIPOS_DATOS/src/main/java/com/mycompany/eva2_12_tipos_datos/EVA2_12_TIPOS_DATOS.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_12_tipos_datos;
+
+/**
+ *
+ * @author fdo_g
+ */
+public class EVA2_12_TIPOS_DATOS {
+
+    public static void main(String[] args) {
+       byte valor=-128;
+       byte valor2 =127;
+        System.out.println("valor ="+ (valor));
+        System.out.println("valor ="+ (valor2));
+        short val=-32768;
+        short val2=32767;
+        int x = -2147483618;
+        int Y = 2147483617;
+       
+    }
+}
